35f4e7412a49301058f7292c8eb09a2bff15f885
Matias Martinelli <54157579+MatiasMart@users.noreply.github.com>
2023-11-11 22:18:39 -0300

    continued to work on the Upcoming page, added text label, imageView and Button

--- MovieHunt/View/TitleTableViewCell.swift
+++ MovieHunt/View/TitleTableViewCell.swift
@@ -17,14 +17,13 @@ class TitleTableViewCell: UITableViewCell {
         return button
     }()
     
-    private let imageLabel: UILabel = {
+    private let titleLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private let titlePosterUIImageView: UIImageView = {
-        
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
         return imageView
@@ -32,8 +31,8 @@ class TitleTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.addSubview(titlePosterUIImageView )
-        contentView.addSubview(imageLabel)
+        contentView.addSubview(titlePosterUIImageView)
+        contentView.addSubview(titleLabel)
         contentView.addSubview(playTitleButton)
 
         applyConstraints()
@@ -42,16 +41,27 @@ class TitleTableViewCell: UITableViewCell {
     private func applyConstraints() {
         let titlePosterUIImageViewConstraints = [
             titlePosterUIImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            titlePosterUIImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
-            titlePosterUIImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15),
-            titlePosterUIImageView.widthAnchor.constraint(equalToConstant: 100)
+//            titlePosterUIImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
+//            titlePosterUIImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15),
+//            titlePosterUIImageView.widthAnchor.constraint(equalToConstant: 100)
         ]
         
-        NSLayoutConstraint.activate(titlePosterUIImageViewConstraints )
+        let titleLabelConstraints = [
+            titleLabel.leadingAnchor.constraint(equalTo: titlePosterUIImageView.trailingAnchor, constant: 20),
+            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(titlePosterUIImageViewConstraints)
+        NSLayoutConstraint.activate(titleLabelConstraints)
     }
     
     public func configure(with model: TitleViewModel) {
-         
+        
+        guard let url = URL(string: "https://image.tmdb.org/t/p/w500/\(model.posterURL)") else {return}
+
+        titlePosterUIImageView.sd_setImage(with: url, completed: nil)
+        titleLabel.text = model.titleName
+        
     }
 
     
---
+++ MovieHunt/ViewModel/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  MovieHunt
+//
+//  Created by Matias Martinelli on 11/11/2023.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String 
+}
